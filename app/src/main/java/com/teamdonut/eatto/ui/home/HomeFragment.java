3375ee3fe802a1d21adcec13781d3fe80fefd5a2
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-01-30 17:11:34 +0900

    #15-HomeFragement class 생성

---
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.teamdonut.eatto.ui.home;
+
+import androidx.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.teamdonut.eatto.R;
+
+public class HomeFragment extends Fragment {
+    private HomeViewModel mViewModel;
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
