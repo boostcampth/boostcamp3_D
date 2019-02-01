762651e5ec14777525ce57cc50a849ddcf9ea5af
wjddnwls918 <wjddnwls918@naver.com>
2019-02-01 10:55:08 +0900

    #20-review 수정 작업(테스트용 로그 제거,Constraint layout사용, 설정, View 속성, 불필요 함수 제거)

--- app/src/main/java/com/teamdonut/eatto/ui/map/MapFragment.java
+++ app/src/main/java/com/teamdonut/eatto/ui/map/MapFragment.java
@@ -6,20 +6,23 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.teamdonut.eatto.R;
 import com.teamdonut.eatto.databinding.MapFragmentBinding;
+
 import net.daum.mf.map.api.MapView;
 
 
 public class MapFragment extends Fragment {
 
-    private MapFragmentBinding mapFragmentBinding;
+    private MapFragmentBinding binding;
     private MapViewModel mViewModel;
 
     public static MapFragment newInstance() {
@@ -41,44 +44,18 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mapFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.map_fragment,container,false);
-        View view = mapFragmentBinding.getRoot();
+        binding = DataBindingUtil.inflate(inflater, R.layout.map_fragment, container, false);
+        View view = binding.getRoot();
 
         mViewModel = new MapViewModel();
-        mapFragmentBinding.setViewmodel(mViewModel);
+        binding.setViewmodel(mViewModel);
 
-        //get AppKey
-        String appKey= getAppKey(getContext());
-
+        //레이아웃에 지도 추가
         MapView mapView = new MapView(getActivity());
-        mapFragmentBinding.flMapView.addView(mapView);
-
-
+        binding.flMapView.addView(mapView);
 
         return view;
     }
-
-
-    private String getAppKey(Context context) {
-        String apiKey = null;
-
-        try {
-            String e = context.getPackageName();
-            ApplicationInfo ai = context
-                    .getPackageManager()
-                    .getApplicationInfo(e, PackageManager.GET_META_DATA);
-            Bundle bundle = ai.metaData;
-            if(bundle != null) {
-                apiKey = bundle.getString("com.kakao.sdk.AppKey");
-            }
-        } catch (Exception e) {
-            Log.d("getAppKeyError", "Caught non-fatal exception while retrieving apiKey: " + e);
-        }
-
-        return apiKey;
-    }
-
-
 
 
 }
--- app/src/main/java/com/teamdonut/eatto/ui/map/MapViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/map/MapViewModel.java
@@ -7,22 +7,17 @@ import androidx.databinding.BindingMethod;
 public class MapViewModel {
 
     public MapViewModel() {
-
-
     }
 
 
     //검색 버튼 리스너
     public void onClickSearchButton(View view){
 
-
-        Log.d("searchButton","hihihi");
     }
 
 
     //게시물 추가 리스너
     public void onClickBoardAdd(View view) {
-        Log.d("boardAdd","hihii");
 
     }
 }
