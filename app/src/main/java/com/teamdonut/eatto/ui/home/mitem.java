fd23af0949cf998e6fe062a87903912296a26eff
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-01 14:17:41 +0900

    cxvxcv

---
+++ app/src/main/java/com/teamdonut/eatto/ui/home/mitem.java
@@ -0,0 +1,11 @@
+package com.teamdonut.eatto.ui.home;
+
+public class mitem {
+    public int drawableId;
+    public String price;
+
+    public mitem(int drawableId, String price){
+        this.drawableId = drawableId;
+        this.price = price;
+    }
+}
--- app/src/main/java/com/teamdonut/eatto/ui/map/MapFragment.java
+++ app/src/main/java/com/teamdonut/eatto/ui/map/MapFragment.java
@@ -1,15 +1,6 @@
 package com.teamdonut.eatto.ui.map;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,14 +10,15 @@ import com.teamdonut.eatto.databinding.MapFragmentBinding;
 
 import net.daum.mf.map.api.MapView;
 
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
 
 public class MapFragment extends Fragment {
 
     private MapFragmentBinding binding;
     private MapViewModel mViewModel;
-
     public static MapFragment newInstance() {
-
         Bundle args = new Bundle();
 
         MapFragment fragment = new MapFragment();
@@ -37,7 +29,6 @@ public class MapFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -46,16 +37,12 @@ public class MapFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.map_fragment, container, false);
         View view = binding.getRoot();
-
         mViewModel = new MapViewModel();
         binding.setViewmodel(mViewModel);
-
         //레이아웃에 지도 추가
         MapView mapView = new MapView(getActivity());
         binding.flMapView.addView(mapView);
 
         return view;
     }
-
-
 }
