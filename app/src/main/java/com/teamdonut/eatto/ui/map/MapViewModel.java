d9c71d6ecd25786a1fa53aa9b4519682846058af
wjddnwls918 <wjddnwls918@naver.com>
2019-02-06 18:35:32 +0900

    #57-게시글 작성 이벤트 연결

--- app/src/main/java/com/teamdonut/eatto/ui/map/MapViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/map/MapViewModel.java
@@ -1,12 +1,24 @@
 package com.teamdonut.eatto.ui.map;
 
+import android.content.Context;
 import android.view.View;
 
 public class MapViewModel {
 
-    public MapViewModel() {
+    private Context mContext;
+    private MapNavigator mNavigator;
+
+    public MapViewModel(Context context) {
+        mContext = context.getApplicationContext();
     }
 
+    void setNavigator(MapNavigator navigator) {
+        mNavigator = navigator;
+    }
+
+    void onFragmentDestroyed() {
+        mNavigator = null;
+    }
 
     //검색 버튼 리스너
     public void onClickSearchButton(View view) {
@@ -16,6 +28,8 @@ public class MapViewModel {
 
     //게시물 추가 리스너
     public void onClickBoardAdd(View view) {
-
+        if (mNavigator != null) {
+            mNavigator.addBoard();
+        }
     }
 }
