e3eb74f8b5bed82fd79c077cf36ec2b121ffe760
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-11 17:07:03 +0900

    #72-callback 초기화 위치 변경

--- app/src/main/java/com/teamdonut/eatto/common/util/GpsModule.java
+++ app/src/main/java/com/teamdonut/eatto/common/util/GpsModule.java
@@ -4,7 +4,13 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.Location;
 import android.os.Looper;
-import com.google.android.gms.location.*;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.teamdonut.eatto.ui.main.MainActivity;
 import com.teamdonut.eatto.ui.map.MapNavigator;
 
 import java.lang.ref.WeakReference;
@@ -13,35 +19,39 @@ public class GpsModule {
     private LocationRequest locationRequest;
     private static final long UPDATE_INTERVAL = 15000, FASTEST_INTERVAL = 10000;
     private FusedLocationProviderClient mFusedLocationClient;
-    private LocationCallback mLocationCallback;
+    private MapNavigator mapNavigator;
     private final Context mContext;
-    public GpsModule(WeakReference<Context> context, MapNavigator mapNavigator){
-        mContext = context.get();
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mContext);
-        mLocationCallback = new LocationCallback(){
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                if (locationResult == null) {
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    if(location != null){
-                        ActivityUtils.saveStrValueSharedPreferences(mContext, "gps", "longitude", String.valueOf(location.getLongitude()));
-                        ActivityUtils.saveStrValueSharedPreferences(mContext, "gps", "latitude", String.valueOf(location.getLatitude()));
-                        stopLocationUpdates();
+    private LocationCallback mLocationCallback =  mLocationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            if (locationResult == null) {
+                return;
+            }
+            for (Location location : locationResult.getLocations()) {
+                if(location != null){
+                    ActivityUtils.saveStrValueSharedPreferences(mContext, "gps", "longitude", String.valueOf(location.getLongitude()));
+                    ActivityUtils.saveStrValueSharedPreferences(mContext, "gps", "latitude", String.valueOf(location.getLatitude()));
+                    stopLocationUpdates();
+                    if(mContext instanceof MainActivity) {
                         mapNavigator.setMyPosition();
                     }
                 }
             }
-        };
+        }
+    };
+
+    public GpsModule(WeakReference<Context> context, MapNavigator mapNavigator){
+        mContext = context.get();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mContext);
+        this.mapNavigator = mapNavigator;
         locationRequest = new LocationRequest()
                 .setFastestInterval(FASTEST_INTERVAL)
                 .setInterval(UPDATE_INTERVAL)
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    public void stopLocationUpdates(){
+    private void stopLocationUpdates(){
         if (mFusedLocationClient != null) {
             mFusedLocationClient.removeLocationUpdates(mLocationCallback);
         }
