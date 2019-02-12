0523ad11215f6ba39609bdf682680a25a28bc0a6
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-12 15:05:05 +0900

    #76-baseadapter에 맞춰서 수정

--- app/src/main/java/com/teamdonut/eatto/ui/home/RankingAdapter.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/RankingAdapter.java
@@ -1,10 +1,8 @@
 package com.teamdonut.eatto.ui.home;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 import com.teamdonut.eatto.R;
@@ -15,29 +13,23 @@ import com.teamdonut.eatto.databinding.RankingItemBinding;
 import java.util.List;
 
 public class RankingAdapter extends BaseRecyclerViewAdapter<User, RankingAdapter.ViewHolder> {
-    Context mContext;
-    List<User> items;
-
-    public RankingAdapter(Context context, List<User> dataSet) {
+    public RankingAdapter(List<User> dataSet) {
         super(dataSet);
-        mContext = context;
-        items = dataSet;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView =  LayoutInflater.from(mContext).inflate(R.layout.ranking_item, parent, false);
+        View itemView =  LayoutInflater.from(parent.getContext()).inflate(R.layout.ranking_item, parent, false);
         return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ViewHolder viewHolder = (ViewHolder)holder;
-        viewHolder.binding.setUser(items.get(position));
+    public void onBindView(ViewHolder holder, int position) {
+        holder.binding.setUser(getItem(position));
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        RankingItemBinding binding;
+        public RankingItemBinding binding;
         public ViewHolder(View itemView) {
             super(itemView);
             binding = DataBindingUtil.bind(itemView);
