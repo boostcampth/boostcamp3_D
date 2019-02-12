d940f438a9065d9c3c8598e726fd4a76e0a9bab6
wjddnwls918 <wjddnwls918@naver.com>
2019-02-12 23:13:49 +0900

    #82-Board_Search 액티비티

---
+++ app/src/main/java/com/teamdonut/eatto/ui/board/BoardSearchActivity.java
@@ -0,0 +1,45 @@
+package com.teamdonut.eatto.ui.board;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.teamdonut.eatto.R;
+import com.teamdonut.eatto.common.util.EditTextUtil;
+import com.teamdonut.eatto.data.Board;
+import com.teamdonut.eatto.databinding.BoardSearchActivityBinding;
+
+public class BoardSearchActivity extends AppCompatActivity implements BoardNavigator {
+
+    BoardSearchActivityBinding binding;
+    BoardViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.board_search_activity);
+        mViewModel = new BoardViewModel(this);
+        initToolbar();
+    }
+
+    public void initToolbar() {
+        //setting Toolbar
+        setSupportActionBar(binding.tbBoardSearch);
+
+        //Toolbar nav button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
