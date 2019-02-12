31751589b55643647dacd871f259adcc179c093a
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-12 20:28:28 +0900

    #98-변수명 변경

--- app/src/main/java/com/teamdonut/eatto/ui/board/BoardPreviewDialog.java
+++ app/src/main/java/com/teamdonut/eatto/ui/board/BoardPreviewDialog.java
@@ -1,52 +1,43 @@
 package com.teamdonut.eatto.ui.board;
 
 
-
 import android.app.Dialog;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
+
+import com.teamdonut.eatto.R;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import com.teamdonut.eatto.R;
 
 public class BoardPreviewDialog extends DialogFragment {
 
     @Override
     public void onStart() {
         super.onStart();
-
         Dialog dialog = getDialog();
-        if(dialog != null) {
+        if (dialog != null) {
             int height = ViewGroup.LayoutParams.MATCH_PARENT;
-
-            dialog.getWindow().setLayout(900 , height);
-
-            // remove dialog background
+            dialog.getWindow().setLayout(900, height);
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         }
-
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setStyle(DialogFragment.STYLE_NO_TITLE, R.style.DialogTheme);
-
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View v = inflater.inflate(R.layout.board_preview_dialog, container, false);
-
-        return v;
+        View view = inflater.inflate(R.layout.board_preview_dialog, container, false);
+        return view;
     }
 
 }
