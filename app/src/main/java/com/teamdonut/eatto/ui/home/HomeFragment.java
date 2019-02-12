96b40bd1ec3dc802218c2744251066cda32b51d2
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-12 14:18:46 +0900

    #61-추천 게시글 recyclerview 초기화

--- app/src/main/java/com/teamdonut/eatto/ui/home/HomeFragment.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.teamdonut.eatto.ui.home;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.teamdonut.eatto.R;
 import com.teamdonut.eatto.databinding.HomeFragmentBinding;
 
@@ -25,6 +28,23 @@ public class HomeFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false);
         mViewModel = new HomeViewModel();
         binding.setViewmodel(mViewModel);
-        return  binding.getRoot();
+        initRecommendBoardRv();
+        return binding.getRoot();
     }
+
+    void initRecommendBoardRv() {
+        RecyclerView.LayoutManager recommendBoardManager = new LinearLayoutManager(this.getContext()) {
+            @Override
+            public boolean checkLayoutParams(RecyclerView.LayoutParams lp) {
+                int dp = (int) (getResources().getDimension(R.dimen.space_medium_margin) / getResources().getDisplayMetrics().density);
+                float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp * 3, getResources().getDisplayMetrics());
+                lp.width = (getWidth() - (int) px) / 2;
+                return super.checkLayoutParams(lp);
+            }
+        };
+        ((LinearLayoutManager) recommendBoardManager).setOrientation(LinearLayoutManager.HORIZONTAL);
+        binding.rvRecommendBoard.setHasFixedSize(true);
+        binding.rvRecommendBoard.setLayoutManager(recommendBoardManager);
+    }
+
 }
