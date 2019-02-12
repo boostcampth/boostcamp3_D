c1b9afaabfcf58b2e71048fa9b3069aa6b17b44c
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-12 15:12:47 +0900

    #61-baseadapter에 맞게 수정

--- app/src/main/java/com/teamdonut/eatto/ui/home/RecommendBoardAdapter.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/RecommendBoardAdapter.java
@@ -1,10 +1,8 @@
 package com.teamdonut.eatto.ui.home;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 import com.teamdonut.eatto.R;
@@ -15,25 +13,19 @@ import com.teamdonut.eatto.databinding.RecommendBoardItemBinding;
 import java.util.List;
 
 public class RecommendBoardAdapter extends BaseRecyclerViewAdapter<Board, RecommendBoardAdapter.ViewHolder> {
-    Context mContext;
-    List<Board> items;
-
-    public RecommendBoardAdapter(Context context, List<Board> dataSet) {
+    public RecommendBoardAdapter(List<Board> dataSet) {
         super(dataSet);
-        mContext = context;
-        items = dataSet;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView =  LayoutInflater.from(mContext).inflate(R.layout.recommend_board_item, parent, false);
+        View itemView =  LayoutInflater.from(parent.getContext()).inflate(R.layout.recommend_board_item, parent, false);
         return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ViewHolder viewHolder = (ViewHolder)holder;
-        viewHolder.binding.setBoard(items.get(position));
+    public void onBindView(ViewHolder holder, int position) {
+        holder.binding.setBoard(getItem(position));
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
