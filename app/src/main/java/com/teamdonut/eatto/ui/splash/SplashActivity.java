73dbb6a997ffb3d1e2887d44faee03ec426d786b
Ji-Yun <jiyun.lee0413@gmail.com>
2019-02-12 11:00:51 +0900

    #86-삭제된 내용 복구 6442bc8

---
+++ app/src/main/java/com/teamdonut/eatto/ui/splash/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.teamdonut.eatto.ui.splash;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.teamdonut.eatto.R;
+import com.teamdonut.eatto.common.util.GpsModule;
+import com.teamdonut.eatto.ui.login.LoginActivity;
+import com.tedpark.tedpermission.rx2.TedRx2Permission;
+
+import java.lang.ref.WeakReference;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        TedRx2Permission.with(this)
+                .setDeniedMessage(R.string.permission_reject)
+                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .request()
+                .subscribe(tedPermissionResult -> {
+                    if (tedPermissionResult.isGranted()) {
+                        GpsModule gpsModule = new GpsModule(new WeakReference<Context>(this), null);
+                        gpsModule.startLocationUpdates();
+                    }
+                    new Handler().postDelayed(() -> {
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    },SPLASH_TIME);
+                }, throwable -> {
+                });
+    }
+}
