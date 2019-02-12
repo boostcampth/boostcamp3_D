47a9c7ba5bc61fdf5708ac8a4dabe3f0728aad8a
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-12 13:57:47 +0900

    #76-구분선 추가를 위한 itemDecorator class 생성

--- app/src/main/java/com/teamdonut/eatto/ui/home/HomeFragment.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeFragment.java
@@ -1,13 +1,19 @@
 package com.teamdonut.eatto.ui.home;
 
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.teamdonut.eatto.R;
 import com.teamdonut.eatto.databinding.HomeFragmentBinding;
 
@@ -25,8 +31,8 @@ public class HomeFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false);
         mViewModel = new HomeViewModel();
         binding.setViewmodel(mViewModel);
-        return  binding.getRoot();
         initRankRv();
+        return binding.getRoot();
     void initRankRv(){
         RecyclerView.LayoutManager rankingManager = new LinearLayoutManager(this.getContext()){
             @Override
@@ -40,5 +46,31 @@ public class HomeFragment extends Fragment {
         binding.rvRank.setHasFixedSize(true);
         binding.rvRank.setLayoutManager(rankingManager);
     }
+
+    public class DividerItemDecorator extends RecyclerView.ItemDecoration {
+        private Drawable mDivider;
+        public DividerItemDecorator(Drawable divider) {
+            mDivider = divider;
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+            int space = (int)(parent.getWidth() * 0.03);
+            int dividerLeft = space + parent.getPaddingLeft();
+            int dividerRight = parent.getWidth() - parent.getPaddingRight() - space;
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int dividerTop = child.getBottom() + params.bottomMargin;
+                int dividerBottom = dividerTop + mDivider.getIntrinsicHeight();
+
+                mDivider.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom);
+                mDivider.draw(canvas);
+            }
+        }
     }
 }
