379cb5c9ef3358ff59502fbe9b7a91031780aa65
Ji-Yun <jiyun.lee0413@gmail.com>
2019-02-14 01:38:16 +0900

    #77-사용자 정보를 서버로 전송하는 기능 구현.

--- app/src/main/java/com/teamdonut/eatto/ui/mypage/MyPageEditViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/mypage/MyPageEditViewModel.java
@@ -1,15 +1,76 @@
 package com.teamdonut.eatto.ui.mypage;
 
-import androidx.databinding.ObservableField;
+import android.util.Log;
 
-public class MyPageEditViewModel {
+import com.google.android.gms.common.util.Strings;
+import com.teamdonut.eatto.data.User;
+import com.teamdonut.eatto.model.ServiceGenerator;
+import com.teamdonut.eatto.model.UserAPI;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.ObservableInt;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import io.realm.Realm;
+
+public class MyPageEditViewModel extends BaseObservable {
 
     private MyPageEditNavigator mNavigator;
 
-    public final ObservableField<String> userSex = new ObservableField<>();
+    private UserAPI service = ServiceGenerator.createService(UserAPI.class);
+    private CompositeDisposable disposable;
+
+    private Realm realm = Realm.getDefaultInstance();
+
+    private User user = realm.copyFromRealm(realm.where(User.class).findFirst());
+
+    public final ObservableInt userSex = new ObservableInt(user.getSex());
+    public final ObservableInt userAge = new ObservableInt(user.getAge());
 
     public MyPageEditViewModel(MyPageEditNavigator navigator) {
         this.mNavigator = navigator;
+    }
+
+    public void submitUserInformation() {
+        disposable = new CompositeDisposable(
+                service.postUserInfo(user)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(result -> {
+                                    //print response log
+                                }
+                                , e -> {
+                                    e.printStackTrace();
+                                }
+                        ));
+    }
+
+    public void onSubmitUserClick(String nickName) {
+        if (isSubmissionFormIsRight(nickName, userAge.get())) {
+            user.setNickName(nickName);
+            user.setSex(userSex.get());
+            user.setAge(userAge.get());
+
+            submitUserInformation();
+        }
+    }
+
+    /**
+     * 닉네임과 나이의 형식이 맞는 지 확인한다.
+     */
+    private Boolean isSubmissionFormIsRight(String nickName, int age) {
+        return (age > 14 && age < 81) && !Strings.isEmptyOrWhitespace(nickName);
+    }
+
+    public void onAgeTextChanged(CharSequence s, int start, int before, int count) {
+        String input = s.toString();
+
+        if (!Strings.isEmptyOrWhitespace(input)) {
+            userAge.set(Integer.parseInt(input));
+        } else {
+            userAge.set(0); //invalid(default) user age.
+        }
     }
 
     public void onShowSelectSexDialogClick() {
@@ -20,4 +81,12 @@ public class MyPageEditViewModel {
         mNavigator.selectPhoto();
     }
 
+    public void onDestroyViewModel() {
+        realm.close();
+        disposable.dispose();
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
