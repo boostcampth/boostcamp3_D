c23bcbcfc7fe2c988faf499d95ca7ef4715137a9
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-13 15:25:58 +0900

    #111-BoardAPI로 변경

--- app/src/main/java/com/teamdonut/eatto/model/BoardAPI.java
+++ app/src/main/java/com/teamdonut/eatto/model/BoardAPI.java
@@ -1,16 +1,11 @@
 package com.teamdonut.eatto.model;
 
 import com.teamdonut.eatto.data.Board;
-
 import io.reactivex.Single;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
-public interface BoardAddAPI {
-
+public interface BoardAPI {
     @POST("board")
     Single<Board> addBoard(@Body Board board);
-
 }
