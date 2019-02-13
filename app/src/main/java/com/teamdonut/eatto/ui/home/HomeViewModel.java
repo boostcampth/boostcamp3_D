e6d7017364dde7519c1ee1fc51ce28894bc9ecfd
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-13 15:31:08 +0900

    #111-mutablelivedata 추가

--- app/src/main/java/com/teamdonut/eatto/ui/home/HomeViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeViewModel.java
@@ -1,6 +1,16 @@
 package com.teamdonut.eatto.ui.home;
 
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData;
+import com.teamdonut.eatto.data.User;
+import com.teamdonut.eatto.model.ServiceGenerator;
+import com.teamdonut.eatto.model.UserAPI;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
-public class HomeViewModel extends ViewModel {
+import java.util.ArrayList;
+
+public class HomeViewModel {
+    public MutableLiveData<ArrayList<User>> userList = new MutableLiveData<>();
+
 }
