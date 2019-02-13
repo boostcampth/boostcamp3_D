a1861a69a1cd9fdcccac1e7f1908a4972c5d5a23
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-13 19:51:19 +0900

    #121-추천 게시글 가져오는 기능 추가

--- app/src/main/java/com/teamdonut/eatto/ui/home/HomeViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeViewModel.java
@@ -1,9 +1,10 @@
 package com.teamdonut.eatto.ui.home;
 
 import androidx.lifecycle.MutableLiveData;
+import com.teamdonut.eatto.data.Board;
 import com.teamdonut.eatto.data.User;
-import com.teamdonut.eatto.model.ServiceGenerator;
 import com.teamdonut.eatto.model.HomeAPI;
+import com.teamdonut.eatto.model.ServiceGenerator;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
@@ -12,15 +13,31 @@ import java.util.List;
 
 public class HomeViewModel {
     public MutableLiveData<List<User>> userList = new MutableLiveData<>();
+    public MutableLiveData<List<Board>> boardList = new MutableLiveData<>();
+
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
-    public void fetchRankUsersList(){
+    public void fetchRecommendBoardList() {
+        HomeAPI service = ServiceGenerator.createService(HomeAPI.class);
+        compositeDisposable.add(
+                service.getRecommendBoards().subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(boards -> {
+                                    boardList.setValue(boards);
+                                }, (e) -> {
+                                    e.printStackTrace();
+                                }
+                        )
+        );
+    }
+
+    public void fetchRankUsersList() {
         HomeAPI service = ServiceGenerator.createService(HomeAPI.class);
         compositeDisposable.add(
                 service.getTopTenUsers().subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe((data) -> {
-                                    userList.setValue(data);
+                        .subscribe(users -> {
+                                    userList.setValue(users);
                                 }, (e) -> {
                                     e.printStackTrace();
                                 }
