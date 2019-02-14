61df705a148e7f5ec708a018a40b95e161215dff
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-15 01:39:11 +0900

    #144-mutablelivedata 대신 observablearraylist 사용

--- app/src/main/java/com/teamdonut/eatto/ui/home/HomeViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeViewModel.java
@@ -1,6 +1,7 @@
 package com.teamdonut.eatto.ui.home;
 
-import androidx.lifecycle.MutableLiveData;
+import androidx.databinding.ObservableArrayList;
+import androidx.lifecycle.ViewModel;
 import com.teamdonut.eatto.data.Board;
 import com.teamdonut.eatto.data.User;
 import com.teamdonut.eatto.model.HomeAPI;
@@ -9,11 +10,11 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
-import java.util.List;
-
-public class HomeViewModel {
-    public MutableLiveData<List<User>> userList = new MutableLiveData<>();
-    public MutableLiveData<List<Board>> boardList = new MutableLiveData<>();
+public class HomeViewModel extends ViewModel {
+    public ObservableArrayList<User> users = new ObservableArrayList<>();
+    public ObservableArrayList<Board> boards = new ObservableArrayList<>();
+    public UserRankingAdapter userRankingAdapter = new UserRankingAdapter(users);
+    public BoardRecommendAdapter boardRecommendAdapter = new BoardRecommendAdapter(boards);
     private CompositeDisposable disposables = new CompositeDisposable();
     private HomeAPI service = ServiceGenerator.createService(HomeAPI.class, ServiceGenerator.BASE);
 
@@ -23,7 +24,8 @@ public class HomeViewModel {
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(data -> {
-                                    boardList.setValue(data);
+                                    boards.addAll(data);
+                                    boardRecommendAdapter.notifyDataSetChanged();
                                 }, e -> {
                                     e.printStackTrace();
                                 }
@@ -37,7 +39,8 @@ public class HomeViewModel {
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(data -> {
-                                    userList.setValue(data);
+                                    users.addAll(data);
+                                    userRankingAdapter.notifyDataSetChanged();
                                 }, e -> {
                                     e.printStackTrace();
                                 }
