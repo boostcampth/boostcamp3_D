48848ae87a564b7a7423ea3caa1379735c640cde
wjddnwls918 <wjddnwls918@naver.com>
2019-02-15 19:01:03 +0900

    #123-BoardSearch 화면 작성

---
+++ app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchActivity.java
@@ -0,0 +1,121 @@
+package com.teamdonut.eatto.ui.board.search;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.teamdonut.eatto.R;
+import com.teamdonut.eatto.common.util.EndlessRecyclerViewScrollListener;
+import com.teamdonut.eatto.common.util.HorizontalDividerItemDecorator;
+import com.teamdonut.eatto.databinding.BoardSearchActivityBinding;
+import com.teamdonut.eatto.ui.board.BoardNavigator;
+import com.teamdonut.eatto.ui.board.BoardViewModel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BoardSearchActivity extends AppCompatActivity implements BoardNavigator {
+
+    BoardSearchActivityBinding binding;
+    BoardViewModel mViewModel;
+    LinearLayoutManager boardSearchManager;
+
+    private EndlessRecyclerViewScrollListener scrollListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.board_search_activity);
+        mViewModel = new BoardViewModel(this);
+        binding.setViewmodel(mViewModel);
+
+        initToolbar();
+
+        initSearchResultRv();
+    }
+
+    public void initToolbar() {
+        //setting Toolbar
+        setSupportActionBar(binding.tbBoardSearch);
+
+        //Toolbar nav button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAddressSearchClick() {
+
+        scrollListener.resetState();
+        mViewModel.fetchAddressResult(getResources().getText(R.string.kakao_rest_api_key).toString(), binding.etInputSearchKeyword.getText().toString(), 1, 10);
+
+        hideKeyboard();
+    }
+
+    public void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+    private void initSearchResultRv() {
+        boardSearchManager = new LinearLayoutManager(this);
+        boardSearchManager.setOrientation(RecyclerView.VERTICAL);
+        binding.rvBoardSearch.setLayoutManager(boardSearchManager);
+        binding.rvBoardSearch.addItemDecoration(new HorizontalDividerItemDecorator
+                (ContextCompat.getDrawable(getApplicationContext(), R.drawable.ranking_divider), 0.03));
+
+        scrollListener = new EndlessRecyclerViewScrollListener(boardSearchManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                mViewModel.fetchAddressResult(getResources().getText(R.string.kakao_rest_api_key).toString(), binding.etInputSearchKeyword.getText().toString(), page, 10);
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        binding.rvBoardSearch.addOnScrollListener(scrollListener);
+
+        binding.rvBoardSearch.setAdapter(mViewModel.getmAdapter());
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        mViewModel.compositeDisposableDispose();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onShowSnackBar() {
+        showSnackBar(getCurrentFocus(), R.string.board_search_can_not_find_result);
+    }
+
+    public void showSnackBar(View parentLayout, int resId) {
+        Snackbar snack = Snackbar.make(parentLayout, getResources().getText(resId).toString(), Snackbar.LENGTH_SHORT);
+        View view = snack.getView();
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
+        params.gravity = Gravity.TOP;
+        view.setLayoutParams(params);
+        snack.show();
+    }
+
+
+}
