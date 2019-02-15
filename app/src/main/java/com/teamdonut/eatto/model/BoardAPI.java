66b6b97af92c9c1eeeae6d79fdb51055f3a56a63
wjddnwls918 <wjddnwls918@naver.com>
2019-02-15 17:36:28 +0900

    #123-BoardAPI getAddress 추가

--- app/src/main/java/com/teamdonut/eatto/model/BoardAPI.java
+++ app/src/main/java/com/teamdonut/eatto/model/BoardAPI.java
@@ -1,11 +1,19 @@
 package com.teamdonut.eatto.model;
 
 import com.teamdonut.eatto.data.Board;
+import com.teamdonut.eatto.data.kakao.LocalKeywordSearch;
+
 import io.reactivex.Single;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface BoardAPI {
     @POST("board")
     Single<Board> addBoard(@Body Board board);
+
+    @GET("v2/local/search/keyword.json")
+    Single<LocalKeywordSearch> getAddress(@Header("Authorization") String authorization, @Query("query") String query, @Query("page") int page, @Query("size") int size);
 }
