ee664b87d534acc6ba7caef28cc2cbf7b3094a4f
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-15 12:21:11 +0900

    #144-setAdapter 이름 변경

--- app/src/main/java/com/teamdonut/eatto/common/Binding.java
+++ app/src/main/java/com/teamdonut/eatto/common/Binding.java
@@ -4,7 +4,7 @@ import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class Binding {
-    @BindingAdapter("setAdapter")
+    @BindingAdapter("adapter")
     public static void bindAdapter(RecyclerView recyclerView, RecyclerView.Adapter<?> adapter) {
         recyclerView.setAdapter(adapter);
     }
