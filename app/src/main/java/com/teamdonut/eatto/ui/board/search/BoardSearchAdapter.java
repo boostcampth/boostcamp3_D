f5c45985686f01130b5f124727faec996b2dc189
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-02-16 20:37:34 +0900

    #162-BoardSearchAdapter 수정

--- app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchAdapter.java
+++ app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchAdapter.java
@@ -1,61 +1,44 @@
 package com.teamdonut.eatto.ui.board.search;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.teamdonut.eatto.R;
 import com.teamdonut.eatto.common.BaseRecyclerViewAdapter;
 import com.teamdonut.eatto.data.kakao.Document;
 import com.teamdonut.eatto.databinding.BoardSearchItemBinding;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import androidx.annotation.NonNull;
-import androidx.databinding.BindingAdapter;
-import androidx.databinding.ObservableArrayList;
-import androidx.databinding.library.baseAdapters.BR;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class BoardSearchAdapter extends BaseRecyclerViewAdapter<Document, BoardSearchAdapter.MyViewHolder> {
 
-    private List<Document> documentList;
-
     public BoardSearchAdapter(ArrayList<Document> dataSet) {
         super(dataSet);
-        documentList = dataSet;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        BoardSearchItemBinding binding = BoardSearchItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new MyViewHolder(binding);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.board_search_item, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindView(MyViewHolder holder, int position) {
-        Document document = documentList.get(position);
-        holder.bind(document);
+        holder.binding.setViewmodel(new BoardSearchViewModel(getItem(position)));
     }
 
-    @BindingAdapter("app:item")
-    public static void bindItem(RecyclerView recyclerView, ObservableArrayList<Document> documents) {
-        BoardSearchAdapter adapter = (BoardSearchAdapter) recyclerView.getAdapter();
-        if (adapter != null) {
-            adapter.setItem(documents);
-        }
-    }
-
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         BoardSearchItemBinding binding;
 
-        MyViewHolder(BoardSearchItemBinding binding) {
-            super(binding.getRoot());
-            this.binding = binding;
-        }
-
-        void bind(Document document) {
-            binding.setVariable(BR.document, document);
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            binding = DataBindingUtil.bind(itemView);
         }
     }
 
