d728fc1cc277c628802409102e1a20dd5ddbea38
wjddnwls918 <wjddnwls918@naver.com>
2019-02-16 15:04:22 +0900

    #123-KeyboardUtil 수정

--- app/src/main/java/com/teamdonut/eatto/common/util/KeyboardUtil.java
+++ app/src/main/java/com/teamdonut/eatto/common/util/KeyboardUtil.java
@@ -1,13 +1,14 @@
 package com.teamdonut.eatto.common.util;
 
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 public class KeyboardUtil {
 
-    public static void hideKeyboard(Context context) {
+    public static void hideKeyboard(View view, Context context) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
