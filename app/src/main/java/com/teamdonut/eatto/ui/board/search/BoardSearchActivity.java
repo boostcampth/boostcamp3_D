bc90634be0a5c79a6cc3f74a1a33040c230fed64
wjddnwls918 <wjddnwls918@naver.com>
2019-02-16 13:01:23 +0900

    #123-BoardSearchActivity 수정

--- app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchActivity.java
+++ app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchActivity.java
@@ -1,19 +1,15 @@
 package com.teamdonut.eatto.ui.board.search;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.FrameLayout;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.teamdonut.eatto.R;
 import com.teamdonut.eatto.common.util.EndlessRecyclerViewScrollListener;
 import com.teamdonut.eatto.common.util.HorizontalDividerItemDecorator;
+import com.teamdonut.eatto.common.util.KeyboardUtil;
+import com.teamdonut.eatto.common.util.SnackBarUtil;
 import com.teamdonut.eatto.databinding.BoardSearchActivityBinding;
 import com.teamdonut.eatto.ui.board.BoardNavigator;
 import com.teamdonut.eatto.ui.board.BoardViewModel;
@@ -26,9 +22,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class BoardSearchActivity extends AppCompatActivity implements BoardNavigator {
 
-    BoardSearchActivityBinding binding;
-    BoardViewModel mViewModel;
-    LinearLayoutManager boardSearchManager;
+    private BoardSearchActivityBinding binding;
+    private BoardViewModel mViewModel;
+    private LinearLayoutManager mBoardSearchManager;
 
     private EndlessRecyclerViewScrollListener scrollListener;
 
@@ -82,25 +78,21 @@ public class BoardSearchActivity extends AppCompatActivity implements BoardNavig
                 scrollListener.resetState();
                 mViewModel.fetchAddressResult(getResources().getText(R.string.kakao_rest_api_key).toString(), binding.etInputSearchKeyword.getText().toString(), 1, 10);
 
-                hideKeyboard();
+                KeyboardUtil.hideKeyboard(getApplicationContext());
                 break;
         }
         return true;
     }
-    
-    public void hideKeyboard() {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-    }
+
 
     private void initSearchResultRv() {
-        boardSearchManager = new LinearLayoutManager(this);
-        boardSearchManager.setOrientation(RecyclerView.VERTICAL);
-        binding.rvBoardSearch.setLayoutManager(boardSearchManager);
+        mBoardSearchManager = new LinearLayoutManager(this);
+        mBoardSearchManager.setOrientation(RecyclerView.VERTICAL);
+        binding.rvBoardSearch.setLayoutManager(mBoardSearchManager);
         binding.rvBoardSearch.addItemDecoration(new HorizontalDividerItemDecorator
                 (ContextCompat.getDrawable(getApplicationContext(), R.drawable.ranking_divider), 0.03));
 
-        scrollListener = new EndlessRecyclerViewScrollListener(boardSearchManager) {
+        scrollListener = new EndlessRecyclerViewScrollListener(mBoardSearchManager) {
             @Override
             public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
                 // Triggered only when new data needs to be appended to the list
@@ -114,26 +106,17 @@ public class BoardSearchActivity extends AppCompatActivity implements BoardNavig
         binding.rvBoardSearch.setAdapter(mViewModel.getmAdapter());
     }
 
-
     @Override
     protected void onDestroy() {
-        mViewModel.compositeDisposableDispose();
+        mViewModel.onDestroyBoardViewModel();
         super.onDestroy();
     }
 
     @Override
     public void onShowSnackBar() {
-        showSnackBar(getCurrentFocus(), R.string.board_search_can_not_find_result);
+        SnackBarUtil.showSnackBar(binding.rvBoardSearch, R.string.board_search_can_not_find_result);
     }
 
-    public void showSnackBar(View parentLayout, int resId) {
-        Snackbar snack = Snackbar.make(parentLayout, getResources().getText(resId).toString(), Snackbar.LENGTH_SHORT);
-        View view = snack.getView();
-        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
-        params.gravity = Gravity.TOP;
-        view.setLayoutParams(params);
-        snack.show();
-    }
 
 
 }
