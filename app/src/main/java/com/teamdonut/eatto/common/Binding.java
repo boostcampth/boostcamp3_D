b30d7bb7ec372f2ad9628c47dea6bba5eabb341b
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-18 01:27:09 +0900

    #176-binding image 부분을 util로 이동

--- app/src/main/java/com/teamdonut/eatto/common/Binding.java
+++ app/src/main/java/com/teamdonut/eatto/common/Binding.java
@@ -1,11 +1,22 @@
 package com.teamdonut.eatto.common;
 
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
 import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+import com.teamdonut.eatto.common.util.GlideApp;
 
 public class Binding {
     @BindingAdapter("adapter")
-    public static void bindAdapter(RecyclerView recyclerView, RecyclerView.Adapter<?> adapter) {
+    public static void setAdapter(RecyclerView recyclerView, RecyclerView.Adapter<?> adapter) {
         recyclerView.setAdapter(adapter);
+    }
+
+    @BindingAdapter({"glideImage", "glideError"})
+    public static void setImageFromUrl(ImageView view, String imgUrl, Drawable errorDrawable) {
+        GlideApp.with(view)
+                .load(imgUrl)
+                .error(errorDrawable)
+                .into(view);
     }
 }
