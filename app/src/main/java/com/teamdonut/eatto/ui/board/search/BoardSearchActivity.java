95cdb8dbe19af2c8bc2ce23662ddd1fab7fad479
wjddnwls918 <wjddnwls918@naver.com>
2019-02-18 20:29:15 +0900

    #173-BoardSearchActivity 어뎁터명 변경

--- app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchActivity.java
+++ app/src/main/java/com/teamdonut/eatto/ui/board/search/BoardSearchActivity.java
@@ -73,7 +73,7 @@ public class BoardSearchActivity extends AppCompatActivity implements BoardNavig
                 break;
             case R.id.menu_search:
 
-                mViewModel.getmAdapter().updateItems(mViewModel.getDocuments());
+                mViewModel.getBoardSearchAdapter().updateItems(mViewModel.getDocuments());
 
                 scrollListener.resetState();
                 mViewModel.fetchAddressResult(getResources().getText(R.string.kakao_rest_api_key).toString(), binding.etInputSearchKeyword.getText().toString(), 1, 10);
@@ -103,7 +103,7 @@ public class BoardSearchActivity extends AppCompatActivity implements BoardNavig
         // Adds the scroll listener to RecyclerView
         binding.rvBoardSearch.addOnScrollListener(scrollListener);
 
-        binding.rvBoardSearch.setAdapter(mViewModel.getmAdapter());
+        binding.rvBoardSearch.setAdapter(mViewModel.getBoardSearchAdapter());
     }
 
     @Override
