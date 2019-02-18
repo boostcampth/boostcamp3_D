a782b2625809d324314a4a7f432b1496d641f323
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-18 17:52:51 +0900

    #169-ViewHolder로 이름변경

--- app/src/main/java/com/teamdonut/eatto/ui/map/bottomsheet/MapBoardAdapter.java
+++ app/src/main/java/com/teamdonut/eatto/ui/map/bottomsheet/MapBoardAdapter.java
@@ -3,7 +3,9 @@ package com.teamdonut.eatto.ui.map.bottomsheet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
 import com.teamdonut.eatto.R;
 import com.teamdonut.eatto.common.BaseRecyclerViewAdapter;
 import com.teamdonut.eatto.data.Board;
@@ -12,11 +14,7 @@ import com.teamdonut.eatto.ui.map.MapViewModel;
 
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class MapBoardAdapter extends BaseRecyclerViewAdapter<Board, MapBoardAdapter.MapBoardViewHolder> {
+public class MapBoardAdapter extends BaseRecyclerViewAdapter<Board, MapBoardAdapter.ViewHolder> {
 
     private MapViewModel mViewModel;
 
@@ -28,19 +26,19 @@ public class MapBoardAdapter extends BaseRecyclerViewAdapter<Board, MapBoardAdap
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MapBoardViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.board_item, parent, false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.board_item, parent, false));
     }
 
+
     @Override
-    public void onBindView(MapBoardViewHolder holder, int position) {
+    public void onBindView(ViewHolder holder, int position) {
         holder.binding.setBoard(getItem(position));
     }
 
-    class MapBoardViewHolder extends RecyclerView.ViewHolder {
-
+    public class ViewHolder extends RecyclerView.ViewHolder {
         BoardItemBinding binding;
 
-        MapBoardViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
             binding = DataBindingUtil.bind(itemView);
         }
