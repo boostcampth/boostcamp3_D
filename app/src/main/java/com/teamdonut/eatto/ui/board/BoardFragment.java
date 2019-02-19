56c31b8ae30b82e281dd3e19b29a6c2b009efac9
wjddnwls918 <wjddnwls918@naver.com>
2019-02-19 16:59:08 +0900

    #198-BoardDetail 화면 연결

--- app/src/main/java/com/teamdonut/eatto/ui/board/BoardFragment.java
+++ app/src/main/java/com/teamdonut/eatto/ui/board/BoardFragment.java
@@ -2,11 +2,13 @@ package com.teamdonut.eatto.ui.board;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.teamdonut.eatto.R;
+import com.teamdonut.eatto.data.Board;
 import com.teamdonut.eatto.databinding.BoardFragmentBinding;
 
 import androidx.annotation.NonNull;
@@ -63,6 +65,55 @@ public class BoardFragment extends Fragment implements BoardNavigator {
 
     }
 
+    //보드 상세보기 연결
+
+    @Override
+    public void onShowMyBoardDetail(int position) {
+        Board board = mViewModel.getBoardOwnAdapter().getItem(position);
+        Log.d("send", board.getMaxPerson() + "");
+        Intent intent = new Intent(getContext(), BoardDetailActivity.class);
+        intent.putExtra("addressName", board.getAddress());
+        intent.putExtra("title", board.getTitle());
+        intent.putExtra("restaurantName", board.getRestaurantName());
+        intent.putExtra("minAge", board.getMinAge());
+        intent.putExtra("maxAge", board.getMaxAge());
+        intent.putExtra("budget", board.getBudget());
+        intent.putExtra("appointedTime", board.getAppointedTime().toString());
+        intent.putExtra("content", board.getContent());
+        intent.putExtra("currentPerson", board.getCurrentPerson());
+        intent.putExtra("maxPerson", board.getMaxPerson());
+        intent.putExtra("longitude", board.getLongitude());
+        intent.putExtra("latitude", board.getLatitude());
+        intent.putExtra("writerPhoto", board.getWriterPhoto());
+        intent.putExtra("writerName", board.getWriterName());
+        intent.putExtra("writerId", board.getWriterId());
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void onShowJoinBoardDetail(int position) {
+        Board board = mViewModel.getBoardJoinAdapter().getItem(position);
+        Log.d("send", board.getMaxPerson() + "");
+        Intent intent = new Intent(getContext(), BoardDetailActivity.class);
+        intent.putExtra("addressName", board.getAddress());
+        intent.putExtra("title", board.getTitle());
+        intent.putExtra("restaurantName", board.getRestaurantName());
+        intent.putExtra("minAge", board.getMinAge());
+        intent.putExtra("maxAge", board.getMaxAge());
+        intent.putExtra("budget", board.getBudget());
+        intent.putExtra("appointedTime", board.getAppointedTime().toString());
+        intent.putExtra("content", board.getContent());
+        intent.putExtra("currentPerson", board.getCurrentPerson());
+        intent.putExtra("maxPerson", board.getMaxPerson());
+        intent.putExtra("longitude", board.getLongitude());
+        intent.putExtra("latitude", board.getLatitude());
+        intent.putExtra("writerPhoto", board.getWriterPhoto());
+        intent.putExtra("writerName", board.getWriterName());
+        intent.putExtra("writerId", board.getWriterId());
+
+        startActivity(intent);
+    }
 
     @Override
     public void onDestroy() {
