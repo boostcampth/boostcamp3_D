fe419e5cc77f7a2a7e0f3365cd7a49fd5a38bb8e
Ji-Yun <jiyun.lee0413@gmail.com>
2019-02-20 01:30:31 +0900

    #197-SoftKeyboard보이는 메소드 추가.

--- app/src/main/java/com/teamdonut/eatto/common/util/KeyboardUtil.java
+++ app/src/main/java/com/teamdonut/eatto/common/util/KeyboardUtil.java
@@ -6,9 +6,14 @@ import android.view.inputmethod.InputMethodManager;
 
 public class KeyboardUtil {
 
-    public static void hideKeyboard(View view, Context context) {
+    public static void hideSoftKeyboard(View view, Context context) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static void showSoftKeyboard(View view, Context context) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
     }
 
 }
