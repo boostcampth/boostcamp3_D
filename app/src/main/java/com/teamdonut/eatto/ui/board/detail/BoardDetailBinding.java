4fedb77c29d8f5785bedfe9fde5e144b6fa6b3f3
Ji-Yun <jiyun.lee0413@gmail.com>
2019-02-22 13:04:53 +0900

    #245-게시글 자세히보기 화면 수정.

--- app/src/main/java/com/teamdonut/eatto/ui/board/detail/BoardDetailBinding.java
+++ app/src/main/java/com/teamdonut/eatto/ui/board/detail/BoardDetailBinding.java
@@ -1,12 +1,19 @@
 package com.teamdonut.eatto.ui.board.detail;
 
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.format.DateUtils;
+import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
+
+import com.teamdonut.eatto.R;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import androidx.core.content.ContextCompat;
 import androidx.databinding.BindingAdapter;
 
 public class BoardDetailBinding {
@@ -17,6 +24,7 @@ public class BoardDetailBinding {
         SimpleDateFormat realFormat = new SimpleDateFormat("HH:mm aaa", Locale.US);
         try {
             Date date = format.parse(dateText);
+
             view.setText(realFormat.format(date));
         } catch (ParseException e) {
             e.printStackTrace();
@@ -32,6 +40,20 @@ public class BoardDetailBinding {
             view.setText(realFormat.format(date));
         } catch (ParseException e) {
             e.printStackTrace();
+        }
+    }
+
+    @BindingAdapter({"commentNumber", "mainText"})
+    public static void setSpannableText(TextView view, int number, String mainText) {
+        if (number >= 0) {
+            String numberText = String.valueOf(number);
+
+            SpannableStringBuilder sb = new SpannableStringBuilder(mainText);
+            sb.append(numberText);
+            sb.setSpan(new ForegroundColorSpan(ContextCompat.getColor(view.getContext(), R.color.colorHungryRed))
+                    , mainText.length(), mainText.length()+numberText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            view.setText(sb, TextView.BufferType.SPANNABLE);
         }
     }
 }
