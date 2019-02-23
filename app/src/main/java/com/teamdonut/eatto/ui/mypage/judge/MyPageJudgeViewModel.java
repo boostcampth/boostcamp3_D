23a77b287c88f25a9204885abc1d1433bfc42889
Ji-Yun <jiyun.lee0413@gmail.com>
2019-02-23 16:20:26 +0900

    #251-평가 리스트 구현 부분 수정.

--- app/src/main/java/com/teamdonut/eatto/ui/mypage/judge/MyPageJudgeViewModel.java
+++ app/src/main/java/com/teamdonut/eatto/ui/mypage/judge/MyPageJudgeViewModel.java
@@ -1,41 +1,51 @@
 package com.teamdonut.eatto.ui.mypage.judge;
 
-import androidx.lifecycle.ViewModel;
 import com.google.gson.JsonObject;
+import com.teamdonut.eatto.data.Board;
 import com.teamdonut.eatto.data.model.board.BoardRepository;
+
+import java.util.List;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 import io.reactivex.disposables.CompositeDisposable;
 
-import java.util.ArrayList;
-
 public class MyPageJudgeViewModel extends ViewModel {
-    private JudgeAdapter judgeAdapter = new JudgeAdapter(new ArrayList<>(), this);
 
     private CompositeDisposable disposables = new CompositeDisposable();
 
+    private final MutableLiveData<List<Board>> judgeBoards = new MutableLiveData<>();
+    private final MutableLiveData<Board> submitJudge = new MutableLiveData<>();
+
     private BoardRepository boardRepository = BoardRepository.getInstance();
 
-    public void fetchBoards() {
+
+    public void fetchJudgeBoards() {
         disposables.add(boardRepository.getJudgeBoards()
                 .subscribe(data -> {
-                    judgeAdapter.updateItems(data);
+                    judgeBoards.postValue(data);
                 }));
     }
 
-    public void judgeBoard(long writerId, int boardId, int score) {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("writer_id", writerId);
-        jsonObject.addProperty("board_id", boardId);
-        jsonObject.addProperty("score", score);
-        disposables.add(boardRepository.putJudgeBoards(jsonObject)
+    public void sendJudgeResult(Board board, int score) {
+        JsonObject judge = new JsonObject();
+        judge.addProperty("writer_id", board.getWriterId());
+        judge.addProperty("board_id", board.getId());
+        judge.addProperty("score", score);
+
+        disposables.add(boardRepository.putJudgeBoards(judge)
+                .doAfterSuccess(data -> submitJudge.setValue(board))
                 .subscribe(data -> {
+                }, e -> {
+                    e.printStackTrace();
                 }));
     }
 
-    public void onDestroyViewModel() {
-        disposables.dispose();
+    public MutableLiveData<List<Board>> getJudgeBoards() {
+        return judgeBoards;
     }
 
-    public JudgeAdapter getJudgeAdapter() {
-        return judgeAdapter;
+    public MutableLiveData<Board> getSubmitJudge() {
+        return submitJudge;
     }
 }
