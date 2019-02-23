ef05339964fe0ea3e74e4e54d3ef4ca8f768a95e
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-23 11:45:15 +0900

    #256-custom marker 적용

--- app/src/main/java/com/teamdonut/eatto/data/Keyword.java
+++ app/src/main/java/com/teamdonut/eatto/data/Keyword.java
@@ -2,22 +2,20 @@ package com.teamdonut.eatto.data;
 
 import java.util.Date;
 
-import io.realm.RealmModel;
 import io.realm.RealmObject;
-import io.realm.annotations.RealmClass;
 
 public class Keyword extends RealmObject {
 
-        private String content;
-        private Date searchDate;
+    private String content;
+    private Date searchDate;
 
-        public String getContent() {
-            return content;
-        }
+    public String getContent() {
+        return content;
+    }
 
-        public void setContent(String content) {
-            this.content = content;
-        }
+    public void setContent(String content) {
+        this.content = content;
+    }
 
     public Date getSearchDate() {
         return searchDate;
