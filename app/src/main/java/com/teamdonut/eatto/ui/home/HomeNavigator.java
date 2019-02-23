0b328949235ac31615a674758c7e695c3634984b
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-02-23 19:49:32 +0900

    #264-홈 화면 게시글 클릭 이벤트 추가

--- app/src/main/java/com/teamdonut/eatto/ui/home/HomeNavigator.java
+++ app/src/main/java/com/teamdonut/eatto/ui/home/HomeNavigator.java
@@ -1,5 +1,8 @@
 package com.teamdonut.eatto.ui.home;
 
+import com.teamdonut.eatto.data.Board;
+
 public interface HomeNavigator {
     void goToMapSearch();
+    void openBoardPreview(Board board);
 }
