67d820c2b0d2f59368532e42028e6447eb88e86d
Ji-Yun <jiyun.lee0413@gmail.com>
2019-03-04 16:06:30 +0900

    #251-Edit board search item layout.

---
+++ app/src/main/java/com/teamdonut/eatto/ui/board/search/SearchItemActonListener.java
@@ -0,0 +1,8 @@
+package com.teamdonut.eatto.ui.board.search;
+
+import com.teamdonut.eatto.data.kakao.Document;
+
+public interface SearchItemActonListener {
+
+    void onDocumentClick(Document document);
+}
